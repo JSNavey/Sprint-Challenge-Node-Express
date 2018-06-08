a54630465ee58f74c792c47621f6694d4a374b8c
JSNavey <jsnavey@gmail.com>
2018-06-08 12:45:28 -0400

    implement GET to fetch data to display on postman
    ran into the issue - typo in package.json and an issue about font violation?? Found out that it's becoz Grammarly extension I had in chrome.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const projects = require('./data/helpers/projectModel');
+const projects = require('./data/helpers/projectModel.js');
 const actions = require('./data/helpers/actionModel.js');
 
 const port = 5555;
@@ -10,11 +10,9 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-
-//******************* middleware **************************//
-
-
-//******************* EndPoint Starts Here *******************//
+ const errorAlert = (status, message, res) => {
+    res.status(status).json({ error: message });
+ }
 
 server.get('/', (req, res) => {
     res.send('Hello from Port 5555');
@@ -22,6 +20,16 @@ server.get('/', (req, res) => {
 
 //=================== Projects EndPoint ====================//
 
+server.get('/api/projects', (req, res) => {
+    projects
+        .get()
+        .then(project => {
+            res.json({ project })
+        })
+        .catch(err => {
+            return errorAlert(500, 'The information could not be retieved.', res);
+        })
+})
 
 //=================== Actions EndPoint ====================//
 
